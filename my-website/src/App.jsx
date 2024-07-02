eccf8e4e062fa6f141479ff5749961952f0ad2c8
Amy Fisher <amyfisher@Amys-MacBook-Pro.local>
2024-07-02 18:55:41 +0100

    deleted files

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -3,8 +3,6 @@ import './index.css'
 import { Routes, Route } from "react-router-dom"
 import Home from "./components/Home"
 import Footer from "./components/Footer"
-import About from "./components/About"
-import Projects from './components/Projects'
 
 function App() {
 
@@ -12,8 +10,6 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About /> } />
-        <Route path='/projects' element={<Projects />} />
       </Routes>
       <Footer/>
     </>
